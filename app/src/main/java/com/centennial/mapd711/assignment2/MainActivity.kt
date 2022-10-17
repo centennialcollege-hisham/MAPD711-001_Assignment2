4ec884126a681b3eab2e19e2c8da23cb8fd10da6
Hisham Sanimeh <h.snaimeh93@gmail.com>
2022-10-17 00:02:51 -0400

    init

---
+++ app/src/main/java/com/centennial/mapd711/assignment2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.centennial.mapd711.assignment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
